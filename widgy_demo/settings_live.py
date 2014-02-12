65fba563a70ca5e9a57f9a8f8bc8f6b9801b7f40
Rocky Meza <rocky@fusionbox.com>
2014-02-12 02:02:07 -0700

    Use postgres on live

--- widgy_demo/settings_live.py
+++ widgy_demo/settings_live.py
@@ -5,3 +5,11 @@ TEMPLATE_DEBUG = DEBUG = False
 ALLOWED_HOSTS = [
     'demo.wid.gy',
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'widgy_demo',
+        'USER': 'widgy_demo_user',
+    }
+}
