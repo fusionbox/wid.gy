c128fa0ef25c2f841b9a28bd76f708f5d2d6799e
Rocky Meza <rocky@fusionbox.com>
2014-02-13 02:16:49 -0700

    serve media files locally

--- widgy_demo/urls.py
+++ widgy_demo/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -14,3 +15,10 @@ urlpatterns = patterns('',
 
     url(r'^', include('mezzanine.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    )
