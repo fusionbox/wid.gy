9c5eff8ec8d1b65a1de049aa6f833cedec61e739
Rocky Meza <rocky@fusionbox.com>
2014-04-05 00:33:38 -0600

    Added settings_dev.py

---
+++ widgy_demo/settings_dev.py
@@ -0,0 +1,19 @@
+from .settings import *
+
+TEMPLATE_DEBUG = DEBUG = True
+
+ALLOWED_HOSTS = [
+    'demo.wid.gy',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'widgy_demo',
+        'USER': 'widgy_demo_user',
+    }
+}
+
+FORCE_SCRIPT_NAME = ''
+
+SENTRY_DSN = 'https://16443522c9664b9d985a32633bc190f0:1f4bcdd5a08b412c846e3a2d9f01bdd3@sentry.fusionbox.com/44'
