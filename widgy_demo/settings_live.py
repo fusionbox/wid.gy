99cf9259d891ff939dfd67011d521cba607d9afd
Gavin Wahl <gwahl@fusionbox.com>
2014-04-09 15:38:19 -0600

    live settings

--- widgy_demo/settings_live.py
+++ widgy_demo/settings_live.py
@@ -1,9 +1,10 @@
 from .settings import *
 
-TEMPLATE_DEBUG = DEBUG = True
+TEMPLATE_DEBUG = DEBUG = False
 
 ALLOWED_HOSTS = [
     'demo.wid.gy',
+    'wid.gy',
 ]
 
 DATABASES = {
@@ -17,3 +18,15 @@ DATABASES = {
 FORCE_SCRIPT_NAME = ''
 
 SENTRY_DSN = 'https://16443522c9664b9d985a32633bc190f0:1f4bcdd5a08b412c846e3a2d9f01bdd3@sentry.fusionbox.com/44'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+        'KEY_PREFIX': 'widgy_demo',
+    }
+}
+
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', TEMPLATE_LOADERS),
+)
