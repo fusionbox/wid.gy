6f5bee661caea0daca179b626ff61426ce02031f
Gavin Wahl <gwahl@fusionbox.com>
2014-07-14 13:47:12 -0600

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -5,10 +5,10 @@ from fusionbox.fabric.django import stage, deploy
 
 env.roledefs['live'] = ['fusionbox@demo.wid.gy']
 
-fb_env.virtualenv = 'wid.gy'
+fb_env.virtualenv = 'widgy_marketing'
 fb_env.project_name = 'wid'
 fb_env.tld = '.gy'
-fb_env.vassal = 'wid.gy'
+fb_env.vassal = 'widgy_demo'
 
 stage = roles('dev')(stage)
 deploy = roles('live')(deploy)
